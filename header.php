47eaf8ae3f14dc7bbb12a16223b620e65740f0d9
Victor Desjardins <victordesjardins3@gmail.com>
2023-02-02 11:58:14 -0500

    Titre princicpale de l'entête

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>mon 4W4</title>
-    <a href="<?= bloginfo('url'); ?>"><h1><?= bloginfo('name'); ?> </h1> </a>
+    <a class="site__titre" href="<?= bloginfo('url'); ?>"><h1><?= bloginfo('name'); ?> </h1> </a>
     <h2><?= bloginfo('description'); ?> </h2>
     <?php wp_head() ?>
 </head>
