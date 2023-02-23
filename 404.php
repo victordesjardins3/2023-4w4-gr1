039ac7928c064fc3fad26b8d38db2d3e8acaa414
Victor Desjardins <victordesjardins3@gmail.com>
2023-02-23 12:09:10 -0500

    s5c2 404.php, ajout de cours

---
+++ 404.php
@@ -0,0 +1,16 @@
+<?php 
+/**
+ * index.php est le modèle par défaut du thème 4w4 
+ */
+?>
+
+<?php get_header(); ?>
+    <main>
+        <pre>404.php</pre>
+        <h1>Erreur 404</h1>
+        
+    </main>
+    
+    <?php get_footer(); ?>
+</body>
+</html>
--- category.php
+++ category.php
@@ -5,23 +5,33 @@
 ?>
 
 <?php get_header(); ?>
-    <main>
-        <pre>category.php</pre>
-        <h1>Bienvenue sur 4W4</h1>
-        <section class="blocflex">
-            <?php if(have_posts()):
-                while (have_posts()): the_post(); ?>
-                <article>
-                    <h5><a href="<?php the_permalink(); ?>"><?= wp_trim_words(get_the_title(),4, " ... "); ?></a></h5>
-                    
-                    <hr>
-                    <?= wp_trim_words(get_the_excerpt(), 10, " ... "); ?>
-                </article>
-                <?php endwhile; ?>
-            <?php  endif; ?>
-        </section>
-    </main>
     
+    <main class="site__main">
+   <section class="blocflex">
+      <?php
+      $category = get_queried_object();
+      // Permet de définir la nouvelle requête
+      $args = array(
+         'category_name' => $category->slug,
+         'orderby' => 'title',
+         'order' => 'ASC'
+      );
+      // Création d'une nouvelle requête
+      $query = new WP_Query( $args );
+      // Tout le reste de l'extraction de données est basée 
+      // sur la nouvelle requête contenu dans $query
+      if ( $query->have_posts() ) :
+         while ( $query->have_posts() ) : $query->the_post(); ?>
+            <article>
+               <h2><a href="<?php the_permalink(); ?>"> <?= wp_trim_words(get_the_title(),4, " ... "); ?></a></h2>
+               <p><?= wp_trim_words(get_the_excerpt(), 15) ?></p>
+            </article>
+         <?php endwhile; ?>
+      <?php endif;
+      wp_reset_postdata();?>
+   </section>
+</main>
+
     <?php get_footer(); ?>
 </body>
 </html>
