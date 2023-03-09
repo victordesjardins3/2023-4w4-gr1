9f8537c344abeb08b9bb0db3db1d17b9c338e3e4
Victor Desjardins <victordesjardins3@gmail.com>
2023-03-09 18:16:07 -0500

    Formulation et conception du footer

--- footer.php
+++ footer.php
@@ -1,8 +1,39 @@
 <footer class="site__footer">
 
-    <section class="lien">Lien 1</section>
-    <section class="lien">Lien 2</section>
-    <section class="lien">Lien 3</section>
+    <div class="footer__ensemble">
+    <section class="propostim">
+
+        <h3>À propos du TIM</h3>
+        <p>Infos générales</p>
+        <p>Renseignements sur les profs</p>
+        <p>Grille de cheminement</p>
+
+    </section>
+
+    <section class="reseaux">
+        <h3>Réseaux sociaux</h3>
+        <p>Facebook</p>
+        <p>Instagram</p>
+        <p>Twitter</p>
+    </section>
+
+    <section class="college">
+
+        <h3>Collège de Maisonneuve</h3>
+        <p>Historique</p>
+        <p>Liste des programmes</p>
+        <p>Test d'orientation</p>
+
+    </section>
+
+    <section class="coordonnees">
+        <h3>Coordonnées du collège</h3>
+        <p>3800 R. Sherbrooke E, Montréal, QC</p>
+        <p>H1X 2A2</p>
+        <p>(514) 254-7131</p>
+    </section>
+
+    </div>
 
 </footer>
     <?php wp_footer(); ?>
