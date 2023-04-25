d990c6850fbb71dd3e05a4630adca980d1a6bcc8
Victor Desjardins <victordesjardins3@gmail.com>
2023-04-25 11:58:43 -0400

    s12 Ajout du menu d'evenement et des pages pour ceux-ci

--- front-page.php
+++ front-page.php
@@ -8,7 +8,21 @@
     <main class="site__main">
         <pre class="nompagepre">front-page.php</pre>
         <h1>Bienvenue au Wordpress de 4W4</h1>
+        
+        <h2>Les évènements à venir</h2>
         <section class="blocflex">
+
+            <?php  wp_nav_menu(array( 
+                    "menu" => "evenement",
+                    "container" => "nav",
+                    
+
+            )); ?>
+        </section>
+
+        <section class="blocflex">
+
+
             <?php if(have_posts()):
                 while (have_posts()): the_post(); 
                     $la_categorie = '4w4';
@@ -17,6 +31,9 @@
                     get_template_part("template-parts/categorie",$la_categorie);
                endwhile; 
              endif; ?>
+
         </section>
+
+    
     </main>
 <?php get_footer(); ?>
