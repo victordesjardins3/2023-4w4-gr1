c9d1ea7633eeb0d4851b2564aa6fb8da9fcdcb0c
Victor Desjardins <victordesjardins3@gmail.com>
2023-05-09 10:39:14 -0400

    s14 Ajout d'image thumbnail aux pages publiées

--- template-evenement.php
+++ template-evenement.php
@@ -10,6 +10,7 @@
 <main class="site__main">
 <?php
 if ( have_posts() ) : the_post(); ?>
+<?php the_post_thumbnail('medium'); ?>
 <h1><?= get_the_title(); ?></h1>
 <?php the_content();?>
 <p>L'adresse de l'evénement: <?php the_field('adresse'); ?></p>
