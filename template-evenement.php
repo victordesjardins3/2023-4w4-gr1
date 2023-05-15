c62786798b612052d752222e3c35166a4330ddb0
Victor Desjardins <victordesjardins3@gmail.com>
2023-05-15 18:01:38 -0400

    s15 Ajout d'un champ personnaliser organisateur pour les evenements

--- template-evenement.php
+++ template-evenement.php
@@ -14,7 +14,9 @@ if ( have_posts() ) : the_post(); ?>
 <h1><?= get_the_title(); ?></h1>
 <?php the_content();?>
 <p>L'adresse de l'evénement: <?php the_field('adresse'); ?></p>
-<p>La date et l'heure de l'événement: <?php the_field('date_et_heure'); ?></p>     
+<p>La date et l'heure de l'événement: <?php the_field('date_et_heure'); ?></p>
+<p>Organisateur de l'événement: <?php the_field('organisateur'); ?></p>
+   
 <?php endif;?>
 </main><!-- #main -->
 <?php
