e92882cc7dc046d6d6c902f52cb09ba6ee55f584
Victor Desjardins <victordesjardins3@gmail.com>
2023-05-18 14:09:40 -0400

    s15 soustraction des bordures d'aide

--- functions.php
+++ functions.php
@@ -84,7 +84,7 @@ function add_menu_description_and_thumbnail( $item_output, $item, $depth, $args 
             $post_thumbnail_url = wp_get_attachment_image_src( $post_thumbnail_id, 'thumbnail' );
             $item_output = str_replace( '">' . $args->link_before . $item->title, '">' . $args->link_before . '<span class="title">' . $item->title . '</span><span class="description">' . $item->description . '</span><img src="' . esc_url( $post_thumbnail_url[0] ) . '" class="menu-thumbnail" />', $item_output );
         } else {
-            $item_output = str_replace( '">' . $args->link_before . $item->title, '">' . $args->link_before . '<span class="title">' . $item->title . '</span><br><br><span class="description">' . $item->description . '</span><span class="img__archive"></span>', $item_output );
+            $item_output = str_replace( '">' . $args->link_before . $item->title, '">' . $args->link_before . '<span class="title">' . $item->title . '</span><br><br><span class="description">' . $item->description . '</span><figure class="img__archive"></figure>', $item_output );
         }
     }
     return $item_output;
