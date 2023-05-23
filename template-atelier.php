09e18fd0e22b6cd882704c669ad05c6ef4b73185
Victor Desjardins <victordesjardins3@gmail.com>
2023-05-23 09:19:20 -0400

    ajout du fichier template-atelier

---
+++ template-atelier.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Template name: Evenement
+ */
+
+?>
+
+<?php get_header(); ?>
+<main class="site__main">
+<?php
+if ( have_posts() ) : the_post(); ?>
+<?php the_post_thumbnail('medium'); ?>
+<h1><?= get_the_title(); ?></h1>
+<?php the_content();?>
+<p>L'adresse de l'evénement: <?php the_field('adresse'); ?></p>
+<p>La date et l'heure de l'événement: <?php the_field('date_et_heure'); ?></p>
+<p>Organisateur de l'événement: <?php the_field('organisateur'); ?></p>
+<p>Sigle du cours: <?php the_field('sigle'); ?></p>  
+<?php endif;?>
+</main><!-- #main -->
+<?php
+get_footer();
